46619ec4672b584147534762999995064f97c616
Santhosh <Santhosh@gmail.com>
2024-07-22 15:35:24 +0000

    Automated Report

---
+++ test.py
@@ -0,0 +1,14 @@
+import pandas as pd
+import numpy as np
+import win32com
+import win32com.client
+from datetime import date
+from colorama import win32
+from dateutil.utils import today
+import sys
+import glob
+import pandas as pd
+import csv
+from selenium import webdriver
+import time
+import os
